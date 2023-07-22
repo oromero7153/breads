53d643dae471693a8fb0b1b5a768bd0e475b6b6d
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-07-22 12:26:32 -0700

    completed code along

--- server.js
+++ server.js
@@ -9,9 +9,19 @@ const app = express()
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads!')
-});
+})
 
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
-});
+})
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+  })
+  
+  // Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+  
